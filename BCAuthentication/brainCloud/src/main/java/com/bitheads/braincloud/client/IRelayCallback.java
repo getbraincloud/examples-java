1a36c00eb0bd1d26ac0fa43b8e389b28f1c73ba1
bitheadCody <codym@bitheads.com>
2023-01-04 11:18:07 -0500

    use submodule instead of copying bc library

--- BCAuthentication/brainCloud/src/main/java/com/bitheads/braincloud/client/IRelayCallback.java
+++
@@ -1,5 +0,0 @@
-package com.bitheads.braincloud.client;
-
-public interface IRelayCallback {
-    void relayCallback(int netId, byte[] bytes);
-}
--- BCAuthentication/brainCloud/src/main/java/com/bitheads/braincloud/client/IRelaySystemCallback.java
+++
@@ -1,7 +0,0 @@
-package com.bitheads.braincloud.client;
-
-import org.json.JSONObject;
-
-public interface IRelaySystemCallback {
-    void relaySystemCallback(JSONObject jsonData);
-}
