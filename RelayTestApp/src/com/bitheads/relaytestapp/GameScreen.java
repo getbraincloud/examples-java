6336ded91d7b652ef79a403508b2b9cdfa1106cf
bitheadCody <codym@bitheads.com>
2023-11-14 16:10:03 -0500

    modified coordinate calculation

--- RelayTestApp/src/com/bitheads/relaytestapp/GameScreen.java
+++ RelayTestApp/src/com/bitheads/relaytestapp/GameScreen.java
@@ -58,7 +58,8 @@ class GameScreen extends Screen
                 }
                 int size = shockwave.time * 128 / 30;
                 g.setColor(shockwave.color);
-                g.drawArc(shockwave.pos.x - size / 2, shockwave.pos.y - size / 2, size, size, 0, 360);
+                
+                g.drawArc((int)(shockwave.pos.getX() * 800) - size / 2, (int)(shockwave.pos.getY() * 600) - size / 2, size, size, 0, 360);
             }
 
             // Players' arrows
@@ -67,7 +68,7 @@ class GameScreen extends Screen
                 User member = state.lobby.members.get(i);
                 if (member.pos != null)
                 {
-                    g.drawImage(_cursors[member.colorIndex], member.pos.x, member.pos.y, null);
+                    g.drawImage(_cursors[member.colorIndex], (int)(member.pos.getX() * 800), (int)(member.pos.getY() * 600), null);
                 }
             }
         }
@@ -113,7 +114,7 @@ class GameScreen extends Screen
         JFrame frame = App.getInstance().frame;
         Dimension screenRes = frame.getPreferredSize();
 
-        // Tittle
+        // Title
         {
             JLabel lblTitle = new JLabel("Move mouse around and Click", SwingConstants.CENTER);
             lblTitle.setSize(screenRes.width, screenRes.height / 2 - 350);
@@ -200,13 +201,19 @@ class GameScreen extends Screen
                 @Override
                 public void mouseMoved(MouseEvent e)
                 {
-                    App.getInstance().onPlayerMove(e.getX(), e.getY());
+                    float normalizedX = ((float)e.getX() / (float)playArea.getWidth());
+                    float normalizedY = ((float)e.getY() / (float)playArea.getHeight());
+
+                    App.getInstance().onPlayerMove(normalizedX, normalizedY);
                 }
 
                 @Override
                 public void mouseDragged(MouseEvent e)
                 {
-                    App.getInstance().onPlayerMove(e.getX(), e.getY());
+                    float normalizedX = ((float)e.getX() / (float)playArea.getWidth());
+                    float normalizedY = ((float)e.getY() / (float)playArea.getHeight());
+
+                    App.getInstance().onPlayerMove(normalizedX, normalizedY);
                 }
             });
 
@@ -215,7 +222,10 @@ class GameScreen extends Screen
                 @Override
                 public void mousePressed(MouseEvent e)
                 {
-                    App.getInstance().onPlayerShockwave(e.getX(), e.getY());
+                    float normalizedX = ((float)e.getX() / (float)playArea.getWidth());
+                    float normalizedY = ((float)e.getY() / (float)playArea.getHeight());
+                    
+                    App.getInstance().onPlayerShockwave(normalizedX, normalizedY);
                 }
 
                 @Override
--- RelayTestApp/src/com/bitheads/relaytestapp/Shockwave.java
+++ RelayTestApp/src/com/bitheads/relaytestapp/Shockwave.java
@@ -2,17 +2,18 @@ package com.bitheads.relaytestapp;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.awt.geom.Point2D;
 
 public class Shockwave
 {
     static int shockwaveNextId = 0;
 
-    public Point pos;
+    public Point2D pos;
     public Color color;
     public int id = shockwaveNextId++;
     public int time = 0;
 
-    public Shockwave(Point in_pos, Color in_color)
+    public Shockwave(Point2D in_pos, Color in_color)
     {
         pos = in_pos;
         color = in_color;
