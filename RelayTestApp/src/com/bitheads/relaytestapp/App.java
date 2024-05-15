d7c0d2802eb15972dc7867505be0505a0ade5775
bitheadCody <codym@bitheads.com>
2024-05-15 12:32:03 -0400

    corrected wrapper variable

--- RelayTestApp/src/com/bitheads/relaytestapp/App.java
+++ RelayTestApp/src/com/bitheads/relaytestapp/App.java
@@ -53,7 +53,7 @@ public class App implements IRelayCallback, IRelaySystemCallback
         _bcWrapper = new BrainCloudWrapper();
 
         //TODO Replace values with application IDs
-        //_bc.initialize("appId", "secretKey", "appVersion", "serverUrl");
+        //_bcWrapper.initialize("appId", "secretKey", "appVersion", "serverUrl");
 
         _bcWrapper.getClient().enableLogging(true);
 
